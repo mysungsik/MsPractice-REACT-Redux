96262684303513cb194b7c4b02b166bff8f80aec
cms <audtlr93@naver.com>
2022-12-17 11:44:39 +0900

    redux more

--- 0.Redux-basic/src/components/Auth.js
+++ 0.Redux-basic/src/components/Auth.js
@@ -1,19 +1,28 @@
-import classes from './Auth.module.css';
+import classes from "./Auth.module.css";
+import { useDispatch } from "react-redux";
+import { authAction } from "../store/auth";
 
 const Auth = () => {
+  const authDispatch = useDispatch();
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    authDispatch(authAction.login());
+  };
+
   return (
     <main className={classes.auth}>
       <section>
         <form>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
           </div>
-          <button>Login</button>
+          <button onClick={(e) => loginHandler(e)}>Login</button>
         </form>
       </section>
     </main>
--- 0.Redux-basic/src/components/Header.js
+++ 0.Redux-basic/src/components/Header.js
@@ -1,19 +1,31 @@
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
+import { useDispatch, useSelector } from "react-redux"; // 1. react와 redux 를 연결하는 dispatch 와 selector 을 불러온다.
+import { authAction } from "../store/auth"; // 2. auth State 변경을 위한 authAction 을 불러오고
 
 const Header = () => {
+  const authState = useSelector((state) => state.auth.Authentication); // 3. state 중, "Authentication" State를 불러온다.
+
+  const authDispatch = useDispatch(); // 4. dispatch 사용을 위한, useDispatch 를 호출한다.
+
+  const logoutHandler = () => {
+    authDispatch(authAction.logout());
+    console.log(authState);
+  };
+  console.log(authState);
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
           <li>
-            <a href='/'>My Products</a>
+            <a href="/">My Products</a>
           </li>
           <li>
-            <a href='/'>My Sales</a>
+            <a href="/">My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            {authState && <button onClick={logoutHandler}>Logout</button>}
           </li>
         </ul>
       </nav>
